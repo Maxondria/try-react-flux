6b5cd79896543fb6a2fe42d0bc983ece993202e5
Maxon Tayebwa <maxtayebwa@gmail.com>
2019-09-22 17:32:33 +0300

    [task] add a 404 Not Found Page

--- src/components/common/AppRoutes.js
+++ src/components/common/AppRoutes.js
@@ -1,16 +1,18 @@
 import React from "react";
-import { Route } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import HomePage from "../HomePage";
 import AboutPage from "../AboutPage";
 import CoursesPage from "../CoursesPage";
+import PageNotFound from "./NotFoundPage";
 
 const AppRouter = function() {
   return (
-    <>
+    <Switch>
       <Route path="/" exact component={HomePage} />
       <Route path="/about" component={AboutPage} />
       <Route path="/courses" component={CoursesPage} />
-    </>
+      <Route component={PageNotFound} />
+    </Switch>
   );
 };
 

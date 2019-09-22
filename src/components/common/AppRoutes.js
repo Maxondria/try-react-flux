dd107f91ab8a2f5be26d329bf839bdaa77a56054
Maxon Tayebwa <maxtayebwa@gmail.com>
2019-09-22 21:56:08 +0300

    [task] react router params

--- src/components/common/AppRoutes.js
+++ src/components/common/AppRoutes.js
@@ -4,6 +4,7 @@ import HomePage from "../HomePage";
 import AboutPage from "../AboutPage";
 import CoursesPage from "../CoursesPage";
 import PageNotFound from "./NotFoundPage";
+import ManageCoursePage from "../ManageCourse";
 
 const AppRouter = function() {
   return (
@@ -11,6 +12,7 @@ const AppRouter = function() {
       <Route path="/" exact component={HomePage} />
       <Route path="/about" component={AboutPage} />
       <Route path="/courses" component={CoursesPage} />
+      <Route path="/course/:slug" component={ManageCoursePage} />
       <Redirect path="/about-page" to="about" />
       <Route component={PageNotFound} />
     </Switch>
